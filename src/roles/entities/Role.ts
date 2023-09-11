385793d05b288451bee45137ab7e4ece40dcb174
andreluizsantoss <andre.luiz.itape@gmail.com>
2023-09-11 16:45:39 -0300

    Refatoração RolesRepository TypeORM

--- src/roles/entities/Role.ts
+++ src/roles/entities/Role.ts
@@ -2,13 +2,14 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  IntegerType,
   PrimaryGeneratedColumn,
 } from 'typeorm'
 
 @Entity('roles')
 export class Role {
   @PrimaryGeneratedColumn('increment')
-  id: number
+  id: string
 
   @Column()
   name: string
