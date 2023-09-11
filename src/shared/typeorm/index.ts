417b096b332eaf41938217b356dbcd69c241ad75
andreluizsantoss <andre.luiz.itape@gmail.com>
2023-09-11 16:26:29 -0300

    Migration CreateRoles

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm'
 
 export const dataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT) ?? 3306,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'bd_token',
   entities: ['./src/modules/**/typeorm/entities/*.ts'],
   migrations: ['./src/shared/typeorm/migrations/*.ts'],
 })
---
+++ src/shared/typeorm/migrations/1694455054600-CreateRolesTable.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateRolesTable1694455054600 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'roles',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar(250)',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'datetime',
+            default: 'now()',
+            isNullable: true,
+          },
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('roles')
+  }
+}
